5c1d1b68824ad5679c3ff2ec200b64c0ecc56a1d
devsula50 <devsula50@gmail.com>
2023-09-05 14:06:43 +0900

    react refresher create react app

---
+++ section04/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <h1 title="This works!">
+      Hi, <span>this</span> is ReactJS!
+    </h1>
+  );
+};
+
+// class style component
+/* class App extends React.Component {
+  render() {}
+} */
+
+export default App;
