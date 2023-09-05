00e514f8819bc8a791cfe115d082dc98e6f17f52
devsula50 <devsula50@gmail.com>
2023-09-05 14:37:54 +0900

    using props to pass data between components

--- section04/src/App.js
+++ section04/src/App.js
@@ -2,10 +2,16 @@ import "./App.css";
 import GoalList from "./components/GoalList";
 
 const App = () => {
+  const courseGoals = [
+    { id: "cg1", text: "Finish the Course" },
+    { id: "cg1", text: "Learn all about the Course Main Topic" },
+    { id: "cg1", text: "Help other students in the Course Q&A" },
+  ];
+
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <GoalList />
+      <GoalList goals={courseGoals} />
     </div>
   );
 };
--- section04/src/components/GoalList.js
+++ section04/src/components/GoalList.js
@@ -2,12 +2,12 @@ import React from "react";
 
 import "./GoalList.css";
 
-const GoalList = () => {
+const GoalList = (props) => {
   return (
     <ul className="goal-list">
-      <li>Finish the Course</li>
-      <li>Learn all about the Course Main Topic</li>
-      <li>Help other students in the Course Q&amp;A</li>
+      <li>{props.goals[0].text}</li>
+      <li>{props.goals[1].text}</li>
+      <li>{props.goals[2].text}</li>
     </ul>
   );
 };
